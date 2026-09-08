8fb9b1eb8a0425c6b70598e7b5eaa1fcc121478e
Gabriel <gabriel.ed2003@aluno.ifsc.edu.br>
2026-09-08 10:54:04 -0300

    Fix - wire inexistente utilizado w8, substiruido por w4

--- aula3_ripple_carry/full_adder.v
+++ aula3_ripple_carry/full_adder.v
@@ -12,14 +12,14 @@ module full_adder(
 	//assign co = (a & b) | (ci & b) | (ci & a);
 	
 	//modelagem estrutural
-	wire w1,w2,w3,w4;
+	wire w1,w2,w3;
 	
 	and #2 u1 (w1,a,b);
 	and #2 u2 (w2,ci,b);
 	and #2 u3 (w3,ci,a);
 	or  #3 u4 (co,w1,w2,w3);
 	
-	wire w5,w6,w7,na,nb,nci;
+	wire w4,w5,w6,w7,na,nb,nci;
 	
 	not #1 n1 (na,a);
 	not #1 n2 (nb,b);
@@ -29,6 +29,6 @@ module full_adder(
 	and #3 u6 (w5,na,b,nci);
 	and #3 u7 (w6,na,nb,ci);
 	and #3 u8 (w7,a,nb,nci);
-	or  #4 u9 (s,w5,w6,w7,w8);
+	or  #4 u9 (s,w4,w5,w6,w7);
 	
 endmodule

c9a456e750e7f324e8937d3576553ef135e03d3b
Gabriel <gabriel.ed2003@aluno.ifsc.edu.br>
2026-09-08 09:29:49 -0300

    Modelagem estrutural do somador de 1bit

--- aula3_ripple_carry/full_adder.v
+++ aula3_ripple_carry/full_adder.v
@@ -1,3 +1,4 @@
+`timescale 1ns/1ns
 module full_adder(
 	input a,
 	input b,
@@ -7,7 +8,27 @@ module full_adder(
 );
 
 	//Modelagem data flow
-	assign s = (a & b & ci) | (~a & b & ~ci) | (~a & ~b & ci) | (a & ~b & ~ci);
-	assign co = (a & b) | (ci & b) | (ci & a);
+	//assign s = (a & b & ci) | (~a & b & ~ci) | (~a & ~b & ci) | (a & ~b & ~ci);
+	//assign co = (a & b) | (ci & b) | (ci & a);
+	
+	//modelagem estrutural
+	wire w1,w2,w3,w4;
+	
+	and #2 u1 (w1,a,b);
+	and #2 u2 (w2,ci,b);
+	and #2 u3 (w3,ci,a);
+	or  #3 u4 (co,w1,w2,w3);
+	
+	wire w5,w6,w7,na,nb,nci;
+	
+	not #1 n1 (na,a);
+	not #1 n2 (nb,b);
+	not #1 n3 (nci,ci);
+	
+	and #3 u5 (w4,a,b,ci);
+	and #3 u6 (w5,na,b,nci);
+	and #3 u7 (w6,na,nb,ci);
+	and #3 u8 (w7,a,nb,nci);
+	or  #4 u9 (s,w5,w6,w7,w8);
 	
 endmodule
